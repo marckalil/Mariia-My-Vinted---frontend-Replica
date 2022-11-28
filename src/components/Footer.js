50992e08e285a883085edb07b719f5ff097f2b32
Marc Kalil <marc@marckalil.com>
2022-11-28 11:58:30 +0100

    simplify footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 const Footer = (props) => {
-  return props.color === true ? (
-    <div style={{ backgroundColor: "#ebe9e9" }} className="footer">
+  // Trouver un meilleur nom pour props.color qui doit plutôt exprimer une règle
+  const backgroundColor = props.color ? "#ebe9e9" : "white";
+
+  return (
+    <div style={{ backgroundColor }} className="footer">
       <div className="container">
         <div className="footer-section">
           <span>Politique de Confidentialité</span>
@@ -16,31 +19,11 @@ const Footer = (props) => {
       </div>
       <p style={{ margin: 10 }}>
         Made by <span className="bold">Mariia MERCIER</span> with
-        <span className="bold"> React</span>at
-        <span className="bold"> Le Reacteur</span>
-      </p>
-    </div>
-  ) : (
-    <div style={{ backgroundColor: "white" }} className="footer">
-      <div className="container">
-        <div className="footer-section">
-          <span>Politique de Confidentialité</span>
-          <span className="condition-show">Cookies</span>
-          <span className="hidden-footer">Politique de cookies</span>
-          <span className="hidden-footer">Paramètres des cookies</span>
-          <span>Termes et Conditions</span>
-          <span>Notre plateforme</span>
-          <span className="hidden-footer">Conditions de vente Pro</span>
-          <span className="hidden-footer">Conditions d'utilisation Pro</span>
-          <span className="condition-show">Conditions Pro</span>
-        </div>
-      </div>
-      <p style={{ margin: 10 }}>
-        Made by <span className="bold">Mariia MERCIER</span> with
-        <span className="bold"> React</span>at
+        <span className="bold"> React</span> at
         <span className="bold"> Le Reacteur</span>
       </p>
     </div>
   );
 };
+
 export default Footer;
